f2f64a2c662fdaf125f7ae5fcef9104728566f3e
Suyashkb <122739475+Suyashkb@users.noreply.github.com>
2023-06-14 16:04:42 +0530

    Add files via upload

---
+++ Leap year or not.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    //WAP to check whether the year is leap or not
+    int year;
+
+    // Get user input for year
+    printf("Enter a year: ");
+    scanf("%d", &year);
+
+    // Check if year is a leap year
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+        printf("%d is a leap year.\n", year);
+    } else {
+        printf("%d is not a leap year.\n", year);
+    }
+
+    return 0;
+}
